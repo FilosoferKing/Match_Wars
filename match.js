98f8522120777e01b6c011f31f27e0341f64e0a1
filosoferking <filosoferking@yahoo.com>
2015-07-15 20:28:20 -0700

    Memory Match 0.2

--- match.js
+++ match.js
@@ -1,7 +1,17 @@
 var second_card_clicked = false;
-
+var first_card_click_src = null;
 function click_card(card_back_id, card_front_id) {
 	console.log('Click occured.');
+	console.log('card_front_id is ', card_front_id);
 	$(card_back_id).toggle();
 
+	var card_front_src = $(card_front_id).attr('src');
+	console.log('Src: ', card_front_src);
+
+	/*if(second_card_clicked) {
+		second_card_clicked = true;
+		console.log("Click status: true");
+	} else {
+		if()
+	}*/
 }
