881c7f7f0bf588bec993a423ed6142cc2fee40fe
filosoferking <filosoferking@yahoo.com>
2015-07-16 10:43:26 -0700

    Memory Match v5.4

--- match.js
+++ match.js
@@ -1,16 +1,37 @@
 var second_card_clicked = false;
-var first_card_click_src = null;
-function click_card(card_back_id, card_front_id) {
-	console.log('Click occured.');
-	console.log('card_front_id is ', card_front_id);
-	$(card_back_id).toggle();
+var first_card_front_src = null;
+var first_card_clicked_id = null;
+function click_card(card_back_id) {
+	test = card_back_id;
 
-	var card_front_src = $(card_front_id).attr('src');
-	console.log($('#card_front_1'));
-	console.log('Src: ', card_front_src);
+	card_back_id = $(card_back_id);
+	console.log('Click occured.', card_back_id);
+	
+	card_back_id.toggle();
 
-	if(second_card_clicked) {
+	card_front_id = card_back_id.parent().find('.card_front');
+	var card_front_src = card_front_id.attr('src');
+	console.log('Src: ', first_card_front_src);
+	first_card_clicked_id = card_front_id;
+	first_card_front_src = card_front_id.attr('src');
+
+	if (!second_card_clicked) {
 		second_card_clicked = true;
-		console.log("Second click status: ", second_card_clicked);
-	} 
+		console.log("First card clicked: ", second_card_clicked);
+		second_card_front_src = card_front_id.attr('src');
+		console.log('Second card src: ', second_card_front_src);
+	} else {
+		if (first_card_front_src == second_card_front_src) {
+			console.log('You have a match');
+		} else {
+			console.log("Sorry, not a match.");
+		}
+	}
+
+	/*if () {
+			console.log('You have a match!');
+		} else {
+			console.log('Sorry, not a match');
+		}*/
+	
 }
